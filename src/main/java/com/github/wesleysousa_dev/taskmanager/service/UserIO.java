43414f6aaf475df842128c64bd08593e73e1576e
wesley <wesleydesousa34@gmail.com>
2026-04-02 14:46:01 -0300

    Adição de uma feature e renomeação da classe

--- src/main/java/com/github/wesleysousa_dev/taskmanager/service/UserIO.java
+++ src/main/java/com/github/wesleysousa_dev/taskmanager/service/UserIO.java
@@ -2,7 +2,7 @@ package com.github.wesleysousa_dev.taskmanager.service;
 
 import java.util.Scanner;
 
-public class UserInterface {
+public class UserIO {
     private Scanner scanner = new Scanner(System.in);
 
     public String askStr() {
@@ -13,6 +13,10 @@ public class UserInterface {
         int number = scanner.nextInt();
         scanner.nextLine();
         return number;
+    }
+
+    public void showMessage(String message) {
+        System.out.println(message);
     }
 
 }
