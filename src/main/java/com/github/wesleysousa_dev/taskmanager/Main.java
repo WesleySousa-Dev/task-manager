e3311c9f84a6fbeec6aef431a8819f0bb5b1b56c
wesley <wesleydesousa34@gmail.com>
2026-04-02 14:50:08 -0300

    refactor: reestruturação da classe main

--- src/main/java/com/github/wesleysousa_dev/taskmanager/Main.java
+++ src/main/java/com/github/wesleysousa_dev/taskmanager/Main.java
@@ -1,9 +1,22 @@
 package com.github.wesleysousa_dev.taskmanager;
 
+import com.github.wesleysousa_dev.taskmanager.repository.UserRepository;
+import com.github.wesleysousa_dev.taskmanager.service.*;
+import com.github.wesleysousa_dev.taskmanager.util.JsonManipulator;
+
 public class Main {
     static void main() {
+        JsonManipulator json = new JsonManipulator();
+        UserRepository userRepository = new UserRepository(json);
 
-        UserLogin userLogin = new UserLogin();
-        userLogin.loginMenu();
+        UserIO userInterface = new UserIO();
+        UserLogin userLogin = new UserLogin(userRepository, userInterface);
+        UserRegister userRegister = new UserRegister(userRepository);
+        TaskCreator taskCreator = new TaskCreator(userRepository, userInterface);
+
+        UserInterface menu = new UserInterface(userInterface, userLogin, userRegister, taskCreator);
+
+        menu.startMenu();
+
     }
 }
