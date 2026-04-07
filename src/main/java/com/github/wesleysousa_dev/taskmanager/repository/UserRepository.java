b7224d35d48f432945b55382f1970b7d24ba737f
wesley <wesleydesousa34@gmail.com>
2026-04-07 18:15:18 -0300

    feature: implementação interface

--- src/main/java/com/github/wesleysousa_dev/taskmanager/repository/UserRepository.java
+++ src/main/java/com/github/wesleysousa_dev/taskmanager/repository/UserRepository.java
@@ -1,30 +1,12 @@
 package com.github.wesleysousa_dev.taskmanager.repository;
 
+import com.github.wesleysousa_dev.taskmanager.model.Task;
 import com.github.wesleysousa_dev.taskmanager.model.User;
-import com.github.wesleysousa_dev.taskmanager.util.JsonManipulator;
 
 import java.util.List;
 import java.util.Optional;
 
-public class UserRepository {
-    private JsonManipulator jsonManipulator;
-    private List<User> users;
-
-    public UserRepository(JsonManipulator json) {
-        this.jsonManipulator = json;
-        this.users = jsonManipulator.userList();
-    }
-
-    public List<User> getUsers() {
-        return users;
-    }
-
-    public Optional<User> findByName(String name) {
-        return users.stream().filter(u -> u.getName().equals(name)).findFirst();
-    }
-
-    public void addUser(User user) {
-        users.add(user);
-        jsonManipulator.createJson(users);
-    }
+public interface UserRepository {
+    public Optional<User> findByName(String name);
+    public void addUser(User user);
 }
