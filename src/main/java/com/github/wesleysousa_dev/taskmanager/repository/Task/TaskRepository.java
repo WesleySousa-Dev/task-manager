7e3189f67a5511f5cdfbfa3db7849084b325b140
wesley <wesleydesousa34@gmail.com>
2026-04-07 19:30:03 -0300

    feature: interface TaskRepository

---
+++ src/main/java/com/github/wesleysousa_dev/taskmanager/repository/Task/TaskRepository.java
@@ -0,0 +1,11 @@
+package com.github.wesleysousa_dev.taskmanager.repository.Task;
+
+import com.github.wesleysousa_dev.taskmanager.model.Task;
+import com.github.wesleysousa_dev.taskmanager.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TaskRepository {
+    public List<Task> taskList(User currentUser);
+}
