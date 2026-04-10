4536fe91b074311ebc14cc627ede309bbe45977f
wesley <wesleydesousa34@gmail.com>
2026-04-10 12:36:34 -0300

    refact

--- src/main/java/com/github/wesleysousa_dev/taskmanager/Main.java
+++ src/main/java/com/github/wesleysousa_dev/taskmanager/Main.java
@@ -1,7 +1,9 @@
 package com.github.wesleysousa_dev.taskmanager;
 
-import com.github.wesleysousa_dev.taskmanager.repository.UserJsonRepository;
-import com.github.wesleysousa_dev.taskmanager.repository.UserRepository;
+import com.github.wesleysousa_dev.taskmanager.repository.Task.TaskRepository;
+import com.github.wesleysousa_dev.taskmanager.repository.Task.UserTaskRepository;
+import com.github.wesleysousa_dev.taskmanager.repository.User.UserJsonRepository;
+import com.github.wesleysousa_dev.taskmanager.repository.User.UserRepository;
 import com.github.wesleysousa_dev.taskmanager.service.*;
 import com.github.wesleysousa_dev.taskmanager.util.JsonManipulator;
 
@@ -9,13 +11,15 @@ public class Main {
     static void main() {
         JsonManipulator json = new JsonManipulator();
         UserRepository userRepository = new UserJsonRepository(json);
+        TaskRepository taskRepository = new UserTaskRepository(userRepository);
 
         UserIO userInterface = new UserIO();
         UserLogin userLogin = new UserLogin(userRepository, userInterface);
         UserRegister userRegister = new UserRegister(userRepository);
-        TaskCreator taskCreator = new TaskCreator(userRepository, userInterface);
 
-        UserInterface menu = new UserInterface(userInterface, userLogin, userRegister, taskCreator);
+        TaskCreator taskCreator = new TaskCreator(taskRepository, userInterface);
+
+        UserInterface menu = new UserInterface(userInterface, userLogin, taskRepository ,userRegister, taskCreator);
 
         menu.startMenu();
 
